a4639805204ef03aeee15a10c8e96d821e0b1449
bayuaslamaa <bayuaslamaa@gmail.com>
2020-06-03 19:21:16 +0700

    fix

--- helpers/qrVerify.js
+++ helpers/qrVerify.js
@@ -1,41 +1,43 @@
 const { verifyToken } = require('./jwt')
 const { Queue } = require('../models')
+const { Op } = require('sequelize')
 
 const qrVerify = (req, res, next) => {
-    try {
-        const { token } = req.body
-        const payload = verifyToken(token)
-        if (payload) {
-            Queue.findAll({
-                where: {
-                  [Op.and]: [
-                    { ServiceId: payload.ServiceId },
-                    { status: ['Pending'] }
-                  ]
-                }
-              })
-                .then(response => {
-                  if (response[0].id === payload.id) {
-                      return res.status(200).json(payload)
-                  } else {
-                      return next({
-                          name: 'Forbidden',
-                          errors: [{ message: `Not your queue's turn` }]
-                      })
-                  }
-                })
-                .catch(err => {
-                  return next(err)
-                })
-        } else {
-            return next({
-                name: 'Not Found',
-                errors: [{ message: 'Queue not found!' }]
-            })
+  try {
+    const { token } = req.body
+    const payload = verifyToken(token)
+    // console.log(payload)
+    if (payload) {
+      Queue.findAll({
+        where: {
+          [Op.and]: [
+            { ServiceId: payload.ServiceId },
+            { status: ['Pending'] }
+          ]
         }
-    } catch (err) {
-        return next(err)
+      })
+        .then(response => {
+          if (response[0].id === payload.id) {
+            return res.status(200).json(payload)
+          } else {
+            return next({
+              name: 'Forbidden',
+              errors: [{ message: `Not your queue's turn` }]
+            })
+          }
+        })
+        .catch(err => {
+          return next(err)
+        })
+    } else {
+      return next({
+        name: 'Not Found',
+        errors: [{ message: 'Queue not found!' }]
+      })
     }
+  } catch (err) {
+    return next(err)
+  }
 }
 
 module.exports = qrVerify
